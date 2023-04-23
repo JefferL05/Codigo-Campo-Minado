a65cb180e98705ac83d8a3a2ed9d1d684533671d
Jeffferson Vargas Lopes <jeffersonlopes130@gmail.com>
2023-04-23 18:33:57 -0300

    Campo Minado

---
+++ index.js
@@ -0,0 +1,121 @@
+const grid = document.getElementById("grid");
+let lockGame = false;
+const testMode = false;
+//Defina o modo de teste como verdadeiro se quiser ver a localização das minas
+generateGrid();
+
+// Gerar grade 10 * 10
+function generateGrid() {
+  lockGame = false;
+  grid.innerHTML = "";
+  for (var i = 0; i < 10; i++) {
+    row = grid.insertRow(i);
+    for (var j = 0; j < 10; j++) {
+      cell = row.insertCell(j);
+      cell.onclick = function () {
+        init(this);
+      };
+      var mine = document.createAttribute("mine");
+      mine.value = "false";
+      cell.setAttributeNode(mine);
+    }
+  }
+  generateMines();
+}
+
+//Gerar nota 10 * 10
+function generateMines() {
+  for (var i = 0; i < 10; i++) {
+    var row = Math.floor(Math.random() * 10);
+    var col = Math.floor(Math.random() * 10);
+    var cell = grid.rows[row].cells[col];
+    cell.setAttribute("mine", "true");
+    if (testMode) {
+      cell.innerHTML = "x";
+    }
+  }
+}
+
+// Highlight all mines red
+function revealMines() {
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      var cell = grid.rows[i].cells[j];
+      if (cell.getAttribute("mine") == "true") {
+        cell.className = "mine";
+      }
+    }
+  }
+}
+function checkGameComplete() {
+  var gameComplete = true;
+  for (var i = 0; i < 10; i++) {
+    for (var j = 0; j < 10; j++) {
+      if (
+        grid.rows[i].cells[j].getAttribute("mine") == "false" &&
+        grid.rows[i].cells[j].innerHTML === ""
+      ) {
+        gameComplete = false;
+      }
+    }
+  }
+  if (gameComplete) {
+    alert("Você Ama Todas as Minas!");
+    revealMines();
+  }
+}
+function init(cell) {
+  // Verifique se o jogo foi concluído ou não
+  if (lockGame) {
+    return;
+  } else {
+    // Verifique se o usuário clicou na mina
+    if (cell.getAttribute("mine") === "true") {
+      revealMines();
+      lockGame = true;
+    } else {
+      cell.className = "active";
+      //Exibe o número de minas ao redor da célula
+      var mineCount = 0;
+      var cellRow = cell.parentNode.rowIndex;
+      var cellCol = cell.cellIndex;
+      for (
+        var i = Math.max(cellRow - 1, 0);
+        i <= Math.min(cellRow + 1, 9);
+        i++
+      ) {
+        for (
+          var i = Math.max(cellRow - 1, 0);
+          i <= Math.min(cellRow + 1, 9);
+          i++
+        ) {
+          for (
+            var j = Math.max(cellCol - 1, 0);
+            j <= Math.min(cellCol + 1, 9);
+            j++
+          ) {
+            if (grid.rows[i].cells[j].getAttribute("mine") == "true") {
+              mineCount++;
+            }
+          }
+        }
+        cell.innerHTML = mineCount;
+        if (mineCount == 0) {
+          // if cell don't have mine
+          {
+            for (
+              var j = Math.max(cellRow - 1, 0);
+              j <= Math.min(cellCol + 1, 9);
+              j++
+            ) {
+              if (grid.rows[i].cells[j].innerHTML == "") {
+                init(grid.rows[i].cells[j]);
+              }
+            }
+          }
+        }
+      }
+      checkGameComplete();
+    }
+  }
+}
